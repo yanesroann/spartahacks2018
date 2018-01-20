08ab86cb963977dd9923e0af96d09ba4f372c7c4
Matthew D'Alonzo <matthewdalonzo@Matthews-MacBook-Pro.local>
2018-01-20 11:03:56 -0500

    pachi poochi

--- firebaseextras.js
+++ firebaseextras.js
@@ -14,7 +14,7 @@
   function writeImagedata(imageID) {
     var url_thing = document.getElementById("imgururl").value;
     var title_thing = document.getElementById("phototitle").value;
-    var imageRef = database.ref('images/').once('value');
+    var imageRef = database.ref('images/');
     imageRef.on('value', function(snapshot) {
       var image_id = snapshot.val();
     });
